1e4c6ff5744466ace32494de335cc927d00f392a
Solmaz Khajehpour <solmazkh114@gmail.com>
2025-10-03 15:03:35 -0230

    Create graph object in graph file

--- src/ai/graph/graph.py
+++ src/ai/graph/graph.py
@@ -1,22 +1,14 @@
-from .graph_state import graph, AppState
-from .nodes import get_stock_data_node, moving_average_node
+from .graph_state import AppState
+from .nodes import router_node
+from langgraph.graph import StateGraph
 
-# Create a routing node that decides which action to take
-def router_node(state: AppState) -> AppState:
-    """Router node that calls the appropriate function based on request type"""
-    if state.request_type == "get_stock_data":
-        return get_stock_data_node(state)
-    elif state.request_type == "stock_data_ma":
-        return moving_average_node(state)
-    else:
-        # Default to stock data if no request type specified
-        return get_stock_data_node(state)
 
-# Add only the router node
+# create a graph with state
+graph = StateGraph(AppState)
+
 graph.add_node("router", router_node)
-
 # Set the router as the entry point
 graph.set_entry_point("router")
 
 # Compile the graph
-app = graph.compile()
+compiled_graph = graph.compile()
