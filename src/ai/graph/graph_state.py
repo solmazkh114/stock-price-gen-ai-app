d0248472e10015e99e16ec3706f92f9635905693
Solmaz Khajehpour <solmazkh114@gmail.com>
2025-10-03 15:04:25 -0230

    rename stock_data_ma to moving_average in appState

--- src/ai/graph/graph_state.py
+++ src/ai/graph/graph_state.py
@@ -1,14 +1,10 @@
 from typing import Literal
 from pydantic import BaseModel
-from langgraph.graph import StateGraph
 from ..tools.get_stock_data import GetStockDataInput, GetStockDataOutput
-from ..tools.stock_data_analysis import MovingAverageInput, MovingAverageOutput
-# Define LangGraph state
+from ..tools.moving_average import MovingAverageInput, MovingAverageOutput
 
 class AppState(BaseModel):
-    request_type: Literal["get_stock_data", "stock_data_ma", None] = None
+    request_type: Literal["get_stock_data", "moving_average"] | None = None
     get_stock_data_input: GetStockDataInput | None = None
-    stock_data_ma_input: MovingAverageInput | None = None
+    moving_average_input: MovingAverageInput | None = None
     result: GetStockDataOutput | MovingAverageOutput | None = None
-
-graph = StateGraph(AppState)
