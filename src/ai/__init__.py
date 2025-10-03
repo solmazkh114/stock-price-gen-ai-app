1424a269fcc52cdfd9d3b3584d499d3810f0b05b
Solmaz Khajehpour <solmazkh114@gmail.com>
2025-10-03 15:03:00 -0230

    export compiled graph to support application

---
+++ src/ai/__init__.py
@@ -0,0 +1,11 @@
+"""
+AI Package
+
+This package contains all AI-related functionality including tools and graph processing
+which supports the main application.
+"""
+
+from .graph.graph import compiled_graph
+__all__ = [
+    "compiled_graph"
+]
