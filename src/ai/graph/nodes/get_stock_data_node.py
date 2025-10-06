688377886245c25dda14acdfbaa40551824bc048
Solmaz Khajehpour <solmazkh114@gmail.com>
2025-10-06 12:04:07 -0230

    Delete node folder

--- src/ai/graph/nodes/get_stock_data_node.py
+++
@@ -1,9 +0,0 @@
-from ..graph_state import AppState
-from ...tools.get_stock_data import get_stock_data_function
-from loguru import logger
-
-def get_stock_data_node(state: AppState) -> AppState:
-    """Node for processing stock data requests"""
-    logger.info("get_stock_data_node is called")
-    state.result = get_stock_data_function(state.get_stock_data_input)
-    return state
