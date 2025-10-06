df334aa5d0cf2c09f49408efa2d1411fc0a18e8e
Solmaz Khajehpour <solmazkh114@gmail.com>
2025-10-06 12:06:37 -0230

    Add agent to the graph

---
+++ src/ai/graph/agent_function.py
@@ -0,0 +1,20 @@
+from langchain_openai import ChatOpenAI
+from ..tools.get_stock_data import get_stock_data_tool_function
+from ..tools.moving_average import moving_average_tool_function
+from .agent_state import AgentState
+import os
+from loguru import logger
+from dotenv import load_dotenv
+load_dotenv()
+
+
+llm = ChatOpenAI(api_key= os.environ.get("OPENAI_API_KEY"), model="gpt-4.1-nano", temperature=0)
+tools = [get_stock_data_tool_function, moving_average_tool_function]
+llm_with_tools = llm.bind_tools(tools)
+
+
+def agent_function(state: AgentState):
+    logger.info("Agent is called")
+    messages = state["messages"]
+    response = llm_with_tools.invoke(messages)
+    return {"messages": [response]}
