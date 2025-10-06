6f64ca58990255b2f1b2f7b8fa4dee3772ac707b
Solmaz Khajehpour <solmazkh114@gmail.com>
2025-10-06 12:12:16 -0230

    Define main file for running the application

--- src/main.py
+++ src/main.py
@@ -1,17 +1,8 @@
-from .ai import compiled_graph
+from loguru import logger
+import uvicorn
 
 if __name__ == "__main__":
-    # Example 1: Get today's closing price
-    result1 = compiled_graph.invoke(
-        {"request_type": "get_stock_data",
-        "get_stock_data_input": {"ticker": "AAPL", "data_type": "close", "number_of_days": 5}}
-    )
-    print("result 1:", result1)
+    logger.info("Starting server...")
+    uvicorn.run("src.app:app", host="0.0.0.0", port=8000, reload=True)
 
-    # Example 2: Get last 2 years average close, open
-    result2 = compiled_graph.invoke(
-        {"request_type": "moving_average",
-         "moving_average_input": {"ticker": "AAPL", "window_size": 5, "data_type": ["close", "open"], "years_back": 2}}
-    )
-    print("result 2: ", result2)
 
