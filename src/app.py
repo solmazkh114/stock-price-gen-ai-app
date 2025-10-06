28ff2d070dd1848998add8d7a87b734d40b407ad
Solmaz Khajehpour <solmazkh114@gmail.com>
2025-10-06 12:11:44 -0230

    Define FastAPI application and its reouters

---
+++ src/app.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from loguru import logger
+from .routers.assistant_chat_router import assistant_chat_router
+
+
+
+logger.info("app starting...")
+app = FastAPI(title="Welcome to Stock Price Assistant API")
+
+logger.info("Adding CORS middleware...")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+logger.info("Defining routes...")
+app.include_router(assistant_chat_router, prefix="")
